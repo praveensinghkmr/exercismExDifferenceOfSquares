1817e7d06eb2f94096efb7f5fa2c065ddea17f16
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-07 13:04:14 +0530

    Difference of squares

--- DifferenceOfSquares.cs
+++ DifferenceOfSquares.cs
@@ -24,6 +24,6 @@ public static class DifferenceOfSquares
 
     public static int CalculateDifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return (CalculateSquareOfSum(max) - CalculateSumOfSquares(max));
     }
 }
--- DifferenceOfSquaresTest.cs
+++ DifferenceOfSquaresTest.cs
@@ -34,25 +34,24 @@ public class DifferenceOfSquaresTest
         Assert.Equal(55, DifferenceOfSquares.CalculateSumOfSquares(5));
     }
 
-    [Fact]
-    public void Sum_of_squares_100()
+    [Fact]public void Sum_of_squares_100()
     {
         Assert.Equal(338350, DifferenceOfSquares.CalculateSumOfSquares(100));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Difference_of_squares_1()
     {
         Assert.Equal(0, DifferenceOfSquares.CalculateDifferenceOfSquares(1));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Difference_of_squares_5()
     {
         Assert.Equal(170, DifferenceOfSquares.CalculateDifferenceOfSquares(5));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Difference_of_squares_100()
     {
         Assert.Equal(25164150, DifferenceOfSquares.CalculateDifferenceOfSquares(100));
